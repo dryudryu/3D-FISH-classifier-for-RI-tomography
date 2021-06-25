9e9d39e340ebd4fa0956fb7845901180f1d40580
dryudryu <donghun.ryu29@gmail.com>
2021-06-25 17:02:36 +0900

    Create BaseRunner.py

---
+++ runners/BaseRunner.py
@@ -0,0 +1,37 @@
+import utils
+
+class BaseRunner:
+    def __init__(self, arg, torch_device, logger):
+        self.arg = arg
+        self.torch_device = torch_device 
+        
+        self.model_type = arg.model
+
+        self.epoch = arg.epoch
+        self.start_epoch = 0
+
+        self.batch_size = arg.batch_size
+        
+        self.save_dir = arg.save_dir
+
+        self.logger = logger
+    
+    def save(self):
+        raise NotImplementedError("notimplemented save method")
+
+    def load(self):
+        raise NotImplementedError("notimplemented save method")
+
+    def train(self):
+        raise NotImplementedError("notimplemented save method")
+
+    def valid(self):
+        raise NotImplementedError("notimplemented valid method")
+
+    def test(self):
+        raise NotImplementedError("notimplemented test method")
+
+    def inference(self):
+        raise NotImplementedError("notimplemented interence method")
+        
+        
